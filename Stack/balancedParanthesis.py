ae3e58c79140acb3dd9bef2ee160e1b5bbc9646c
Yash Wagle <ywagle@tibco.com>
2019-06-04 21:20:09 +0530

    multiple paranthesis

--- Stack/balancedParanthesis.py
+++ Stack/balancedParanthesis.py
@@ -19,6 +19,31 @@ def checkBalancedParams(str):
         return False
     return True
 
+def checkParamsAll(Exp):
+    s = Stack()
+    i = 0
+    balanced = True
+    symbol
+    while i < len(str) and balanced == True:
+        if str[i] == '(' or str[i] == '{' or str[i] == '[':
+            s.push(str[i])
+        else:
+            symbol = s.pop()
+            if s.isEmpty():
+                balanced = False
+            elif str[i] == '}' and symbol != '{':
+                balanced = False
+            elif str[i] == ')' and symbol != '(':
+                balanced = False
+            elif str[i] == ']' and symbol != '[':
+                balanced = False
+        i = i + 1
+
+    if balanced == False or s.isEmpty() == True:
+        return False
+    return True
+
+
 
 print(checkBalancedParams('((()()())()))('))
 
